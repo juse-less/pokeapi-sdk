6e2e10da741f332153cbe2cffc066ea4fb48c463
Juse Less <me@juseless.dev>
2023-01-14 22:34:50 +0100

    Added the PokéAPI Connector

---
+++ src/Contracts/PokeApiConnector.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JuseLess\PokeApi\Contracts;
+
+use Saloon\Contracts\Connector;
+
+interface PokeApiConnector extends Connector
+{
+    //
+}
---
+++ src/PokeApiConnector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JuseLess\PokeApi;
+
+use JuseLess\PokeApi\Contracts\PokeApiConnector as PokeApiConnectorContract;
+use JuseLess\PokeApi\Responses\Contracts\Response;
+use Saloon\Http\Connector;
+use Saloon\Traits\Plugins\AcceptsJson;
+
+/**
+ * @method  static  static  make(\Stringable|string $baseUrl = 'https://pokeapi.co/api/v2')
+ */
+class PokeApiConnector extends Connector implements PokeApiConnectorContract
+{
+    use AcceptsJson;
+
+    protected string|null $response = Response::class;
+
+    public function __construct(
+        protected readonly \Stringable|string $baseUrl = 'https://pokeapi.co/api/v2',
+    ) {}
+
+    public function resolveBaseUrl(): string
+    {
+        return (string) $this->baseUrl;
+    }
+}
