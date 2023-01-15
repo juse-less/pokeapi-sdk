3a40edf299cd9908619b7d6cd0296bd5f62881a3
Juse Less <me@juseless.dev>
2023-01-15 23:42:52 +0100

    Retrieve request from current response, not initial response

--- src/Responses/PagedResponse.php
+++ src/Responses/PagedResponse.php
@@ -39,14 +39,24 @@ abstract class PagedResponse extends Response implements PagedPokeApiResponseCon
      */
     public function getIterator(): \Generator
     {
+        // TODO: 1. Abstract away this, so others can benefit from automagic paging,
+        //         regardless of offset/limit, paging number, etc
+        //       2. We need to be able to handle asynchronous and synchronous requests seamlessly.
+        //          Working on figuring out how to determine if initial request was made asynchronously or synchronously,
+        //            and then send the next request based on that.
+        //
+        // Note: Because this is an iterable (\Generator),
+        //         we can pass this immediately to the Pool, as a request generator.
         do {
             $response = $this;
 
             $offset = $response->paging()->offset() ?? RequestPaging::defaultOffset();
             $limit = $response->paging()->limit() ?? RequestPaging::defaultLimit();
 
+            // Clone the request so we don't overwrite the existing values.
+            // We want to resend the exact same request, just bumping the 'paging'.
             /** @var  PagedRequest  $request */
-            $request = clone $this->getRequest();
+            $request = clone $response->getRequest();
 
             $request->query()->merge([
                 'offset' => $offset + $limit,
